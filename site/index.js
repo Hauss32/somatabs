62d7b3dcd3d542b3907066343a58f10666510819
Chris Hauss <chris.a.hauss@gmail.com>
2022-06-30 12:35:17 -0700

    Add useless age verification overlay

--- site/index.js
+++ site/index.js
@@ -1,3 +1,48 @@
 $( () => {
-    
+    const $overlay = createOverlay();
+    const $modal = createOverlayModal();
+    createModalButtons( $modal );
+
+    $( '.button-yes' ).click( function() {
+        $overlay.remove();
+        $modal.remove();
+    })
 })
+
+function createOverlay() {
+    const $overlay = $( '<div></div>' );
+    $overlay.addClass( 'overlay' );
+    $( ' body' ).prepend( $overlay ); 
+
+    return $overlay;
+}
+
+function createOverlayModal() {
+    const $modal = $( '<div></div>');
+    $modal.addClass( 'overlay-modal' );
+
+    const $modalTitle = $( '<h1></h1>');
+    $modalTitle.text('are you are least 21 years of age?');
+
+    $modal.append( $modalTitle );
+
+    $(' body').prepend( $modal );
+
+    return $modal;
+}
+
+function createModalButtons($modal) {
+    const $buttonDiv = $( '<div></div>' );
+
+    $buttonDiv.addClass( 'button-container' );
+
+    const $buttonYes = $( '<p>yes</p>' );
+    $buttonYes.addClass( 'button-yes modal-button' );
+    $buttonDiv.append( $buttonYes );
+
+    const $buttonNo = $( '<p>no</p>' );
+    $buttonNo.addClass( 'button-no modal-button' );
+    $buttonDiv.append($buttonNo);
+
+    $modal.append( $buttonDiv );
+}
